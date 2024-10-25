326875fa2a9580392b200cb47c4107f9cbea4bc1
PlatonovaDarya <platonova_d@icloud.com>
2024-10-25 22:07:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const burgerMenu = document.getElementById('burgerMenu');
+const mobileMenu = document.getElementById('mobileMenu');
+const menuClose = document.getElementById('menuClose');
+
+burgerMenu.addEventListener('click', () => {
+    mobileMenu.style.display = 'block'
+})
+
+menuClose.addEventListener('click', () => {
+    mobileMenu.style.display = 'none'
+})
